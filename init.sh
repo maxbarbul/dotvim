943d869c17e689639c3dc781fe69606f90c64175
Maksym Barbul <maksym.barbul@toatech.com>
2015-06-30 18:47:03 +0300

    [Fix] added Vundle clone to init script

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+set VIM_DIR = ~/.vim
+
 mkdir bundle
-ln -s ~/.vim/etc/_vimrc ~/.vimrc
-ln -s ~/.vim/etc/_gvimrc ~/.gvimrc
+git clone https://github.com/gmarik/Vundle.vim.git bundle/Vundle.vim
+ln -s $VIM_DIR/etc/_vimrc ~/.vimrc
+ln -s $VIM_DIR/etc/_gvimrc ~/.gvimrc
